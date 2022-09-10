146555336bdce1266f73c0d3949bcf691864cf25
chanlee <chanlee@ps-analytics.com>
2022-09-11 00:09:18 +0900

    style: 스프링 진입 클래스명 컨벤션에 맞게 수정
    - `AppInitializer` -> `TicketBookingApplication`

--- app/src/main/java/org/project/TicketBookingApplication.java
+++ app/src/main/java/org/project/TicketBookingApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppInitializer {
+public class TicketBookingApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(AppInitializer.class, args);
+    SpringApplication.run(TicketBookingApplication.class, args);
   }
 }
