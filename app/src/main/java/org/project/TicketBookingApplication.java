20b6d8a0e14d334ca305be6a97de5ba36057a29d
tbonelee <81022113+tbonelee@users.noreply.github.com>
2022-09-11 14:41:38 +0900

    기존 작성된 스프링 진입 클래스명 수정 및 구글-자바 스타일 적용 (#9)
    * style: google-java 스타일 적용
    
    * style: 스프링 진입 클래스명 컨벤션에 맞게 수정
    
    - `AppInitializer` -> `TicketBookingApplication`
    
    Co-authored-by: chanlee <chanlee@ps-analytics.com>

--- app/src/main/java/org/project/TicketBookingApplication.java
+++ app/src/main/java/org/project/TicketBookingApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppInitializer {
-    public static void main(String[] args) {
-        SpringApplication.run(AppInitializer.class, args);
-    }
+public class TicketBookingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TicketBookingApplication.class, args);
+  }
 }
