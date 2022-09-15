1969858a0b3941f87c509b973d8de0855623b955
tbonelee <81022113+tbonelee@users.noreply.github.com>
2022-09-15 09:39:29 +0900

    jwt 유틸 인터페이스 및 구현체 작성 (#15)
    * chore: secrets*.yml -> gitignore에 추가
    
    * feat: access, refresh 토큰에 사용될 JwtUtil 인터페이스 작성
    
    * feat: JwtUtil인터페이스와 jjwt라이브러리를 사용한 구현체 JJwtUtil 클래스 추가
    
    * build: 불필요한 junit 의존성 제거
    
    - spring-boot-starter-test에 이미 JUnit 5.8.2 내장되어 있음 (선웅님 PR 리뷰)
    
    * build: lombok test 의존성 제거 && jjwt 의존성 코드 스타일 정리
    
    - 제거한 후 테스트에서 사용시 오류 발생
    - But, 테스트에서 필요할 일 없을 것으로 생각되어 제거
    
    * test: @SuppressWarning 어노테이션 제거
    
    * test: generateToken() 반환값 not null 테스트 메소드 제거
    
    - not empty 테스트에서 같이 검증되므로 중복
    
    * test: @DisplayName 적용 통해 테스트 메소드 이름 정리
    
    Co-authored-by: chanlee <chanlee@ps-analytics.com>

---
+++ app/src/test/java/org/project/util/JJwtUtilTest.java
@@ -0,0 +1,274 @@
+package org.project.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.time.Instant;
+import java.util.Date;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for methods below.
+ * <p>
+ * String generateToken(Key key, String sub, Date expiration);
+ * <p>
+ * Date getTokenExpirationTime(Key key, String token);
+ * <p>
+ * String getTokenSub(Key key, String token);
+ * <p>
+ * boolean isTokenExpirationBeforeDate(Key key, String token, Date date);
+ * <p>
+ * boolean isTokenSignValid(Key key, String token);
+ */
+public class JJwtUtilTest {
+
+
+  private static final String ACCESS_TOKEN_SECRET = "access_token_secret must be at least 256 bits for HS256 algorithm";
+  private static final String REFRESH_TOKEN_SECRET = "refresh_token_secret must be at least 256 bits for HS256 algorithm";
+
+  private static final Key accessKey = Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes());
+
+  private static final Key refreshKey = Keys.hmacShaKeyFor(REFRESH_TOKEN_SECRET.getBytes());
+
+  private static final Date fakeFixedDate = Date.from(Instant.parse("2021-01-01T00:00:00.00Z"));
+  private JwtUtil jwtUtil = new JJwtUtil();
+
+
+  @Test
+  @DisplayName("generateToken() 반환값이 not empty 이어야 한다.")
+  void generateToken_withValidKeyAndSubAndExpiration_shouldReturnNotEmpty() {
+    final String token = jwtUtil.generateToken(accessKey, "test", fakeFixedDate);
+    assertThat(token).isNotEmpty();
+  }
+
+  @Test
+  @DisplayName("generateToken() 반환값이 not blank 이어야 한다.")
+  void generateToken_withValidKeyAndSubAndExpiration_shouldReturnNotBlank() {
+    final String token = jwtUtil.generateToken(accessKey, "test", fakeFixedDate);
+    assertThat(token).isNotBlank();
+  }
+
+  @Test
+  @DisplayName("generateToken() 반환 토큰의 sub값이 입력한 sub 값과 같아야 한다.")
+  void generateToken_withValidKeyAndSubAndExpiration_shouldReturnTokenWithSameSub() {
+    // given
+    final String sub = "test";
+
+    // when
+    final String token = jwtUtil.generateToken(accessKey, sub, fakeFixedDate);
+
+    // then
+    final String tokenSub = jwtUtil.getTokenSub(accessKey, token);
+    assertThat(tokenSub).isEqualTo(sub);
+  }
+
+  @Test
+  @DisplayName("generateToken() 반환 토큰의 exp값이 입력한 exp 값과 같아야 한다.")
+  void generateToken_withValidKeyAndSubAndExpiration_shouldReturnTokenWithSameExp() {
+    // given
+    final Date exp = fakeFixedDate;
+
+    // when
+    final String token = jwtUtil.generateToken(accessKey, "test", exp);
+
+    // then
+    final Date tokenExp = jwtUtil.getTokenExpirationTime(accessKey, token);
+    assertThat(tokenExp).isEqualTo(exp);
+  }
+
+  @Test
+  @DisplayName("generateToken() 반환 토큰의 서명이 입력한 키로 검증 통과되어야 한다.")
+  void generateToken_withValidKeyAndSubAndExpiration_returnTokenWhichShouldBeValidatedByGivenKey() {
+    // given
+    final Key key = accessKey;
+
+    // when
+    final String token = jwtUtil.generateToken(key, "test", fakeFixedDate);
+
+    // then
+    final boolean isTokenSignValid = jwtUtil.isTokenSignValid(key, token);
+    assertThat(isTokenSignValid).isTrue();
+  }
+
+  @Test
+  @DisplayName("generateToken() 반환 토큰의 서명이 입력하지 않은 키로는 검증 실패해야 한다.")
+  void generateToken_withValidKeyAndSubAndExpiration_returnTokenWhichShouldNotBeValidatedByNotGivenKey() {
+    // given
+    final Key key = accessKey;
+    final Key wrongKey = refreshKey;
+
+    // when
+    final String token = jwtUtil.generateToken(key, "test", fakeFixedDate);
+
+    // then
+    final boolean isTokenSignValid = jwtUtil.isTokenSignValid(wrongKey, token);
+    assertThat(isTokenSignValid).isFalse();
+  }
+
+  @Test
+  @DisplayName("isDateValidForTokenExpClaim()에 넘긴 date시점에 토큰이 유효하면 true를 반환해야 한다.")
+  void isDateValidForTokenExpClaim_withValidTokenAndDate_shouldReturnTrue() {
+    // given
+    final Date exp = Date.from(Instant.parse("2021-01-01T00:00:01Z")); // 만료 시간이 기준 시간보다 1초 뒤
+    final Date dateBeforeExp = Date.from(Instant.parse("2021-01-01T00:00:00Z"));
+    final String token = jwtUtil.generateToken(accessKey, "test", exp);
+
+    // when
+    final boolean isTokenExpirationBeforeDate = jwtUtil.isDateValidForTokenExpClaim(accessKey,
+        token, dateBeforeExp);
+
+    // then
+    assertThat(isTokenExpirationBeforeDate).isTrue();
+  }
+
+  @Test
+  @DisplayName("isDateValidForTokenExpClaim()에 넘긴 date시점에 토큰이 유효하지 않으면 false를 반환해야 한다.")
+  void isDateValidForTokenExpClaim_withValidTokenAndDate_shouldReturnFalse() {
+    // given
+    final Date exp = Date.from(Instant.parse("2021-01-01T00:00:00Z")); // 만료 시간이 기준 시간보다 1초 전
+    final Date dateAfterExp = Date.from(Instant.parse("2021-01-01T00:00:01Z"));
+    final String token = jwtUtil.generateToken(accessKey, "test", exp);
+
+    // when
+    final boolean isTokenExpirationBeforeDate = jwtUtil.isDateValidForTokenExpClaim(accessKey,
+        token, dateAfterExp);
+
+    // then
+    assertThat(isTokenExpirationBeforeDate).isFalse();
+  }
+
+  @Test
+  @DisplayName("isDateValidForTokenExpClaim()에 넘긴 date시점이 토큰의 exp와 같으면 false를 반환해야 한다.")
+  void isDateValidForTokenExpClaim_whenDateAndExpAreTheSame_shouldReturnFalseIfDateIsSameWithTokenExp() {
+    // given
+    final Date exp = Date.from(Instant.parse("2021-01-01T00:00:00Z"));
+    final Date dateEqualToExp = Date.from(Instant.parse("2021-01-01T00:00:00Z"));
+    final String token = jwtUtil.generateToken(accessKey, "test", exp);
+
+    // when
+    final boolean isTokenExpirationBeforeDate = jwtUtil.isDateValidForTokenExpClaim(accessKey,
+        token, dateEqualToExp);
+
+    // then
+    assertThat(isTokenExpirationBeforeDate).isFalse();
+  }
+
+  @Test
+  @DisplayName("isDateValidForTokenExpClaim()에 jwt 토큰이 아닌 문자열을 넘기면 IllegalArgumentException을 던진다.")
+  void isDateValidForTokenExpClaim_withNotJwtToken_shouldThrowIllegalArgumentException() {
+    // given
+    final Date exp = Date.from(Instant.parse("2021-01-01T00:00:00Z"));
+    final Date dateEqualToExp = Date.from(Instant.parse("2021-01-01T00:00:00Z"));
+    final String token = "not a token";
+
+    // when
+    final Throwable throwable = catchThrowable(() -> jwtUtil.isDateValidForTokenExpClaim(accessKey,
+        token, dateEqualToExp));
+
+    // then
+    assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+  }
+
+
+  @Test
+  @DisplayName("getTokenExpirationTime()에 넘긴 토큰의 exp를 반환해야 한다.")
+  void getTokenExpirationTime_withValidToken_shouldReturnTokenExp() {
+    // given
+    final Date exp = fakeFixedDate;
+
+    // when
+    final String token = jwtUtil.generateToken(accessKey, "test", exp);
+
+    // then
+    final Date tokenExp = jwtUtil.getTokenExpirationTime(accessKey, token);
+    assertThat(tokenExp).isEqualTo(exp);
+  }
+
+  @Test
+  @DisplayName("getTokenExpirationTime()에 넘긴 토큰이 jwt 토큰이 아니면 IllegalArgumentException을 던진다.")
+  void getTokenExpirationTime_withNotJwtToken_shouldThrowIllegalArgumentException() {
+    // given
+    final String token = "not a token";
+
+    // when
+    final Throwable throwable = catchThrowable(() -> jwtUtil.getTokenExpirationTime(accessKey,
+        token));
+
+    // then
+    assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("getTokenSub()에 넘긴 토큰의 sub를 반환해야 한다.")
+  void getTokenSub_withValidToken_shouldReturnTokenSub() {
+    // given
+    final String sub = "test";
+
+    // when
+    final String token = jwtUtil.generateToken(accessKey, sub, fakeFixedDate);
+
+    // then
+    final String tokenSub = jwtUtil.getTokenSub(accessKey, token);
+    assertThat(tokenSub).isEqualTo(sub);
+  }
+
+  @Test
+  @DisplayName("getTokenSub()에 넘긴 토큰이 jwt 토큰이 아니면 IllegalArgumentException을 던진다.")
+  void getTokenSub_withNotJwtToken_shouldThrowIllegalArgumentException() {
+    // given
+    final String token = "not a token";
+
+    // when
+    final Throwable throwable = catchThrowable(() -> jwtUtil.getTokenSub(accessKey, token));
+
+    // then
+    assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("isTokenSignValid()에 넘긴 토큰의 서명이 주어진 키에 대해 유효하면 true를 반환해야 한다.")
+  void isTokenSignValid_withValidTokenAndMatchedKey_shouldReturnTrue() {
+    // given
+    final Key key = accessKey;
+    final String token = jwtUtil.generateToken(key, "test", fakeFixedDate);
+
+    // when
+    final boolean isTokenSignValid = jwtUtil.isTokenSignValid(key, token);
+
+    // then
+    assertThat(isTokenSignValid).isTrue();
+  }
+
+  @Test
+  @DisplayName("isTokenSignValid()에 넘긴 토큰의 서명이 주어진 키에 대해 유효하지 않으면 false를 반환해야 한다.")
+  void isTokenSignValid_withValidTokenAndNotMatchedKey_shouldReturnFalse() {
+    // given
+    final Key key = accessKey;
+    final Key wrongKey = refreshKey;
+    final String token = jwtUtil.generateToken(key, "test", fakeFixedDate);
+
+    // when
+    final boolean isTokenSignValid = jwtUtil.isTokenSignValid(wrongKey, token);
+
+    // then
+    assertThat(isTokenSignValid).isFalse();
+  }
+
+  @Test
+  @DisplayName("isTokenSignValid()에 넘긴 토큰이 jwt 토큰이 아니면 IllegalArgumentException을 던진다.")
+  void isTokenSignValid_withNotJwtToken_shouldThrowIllegalArgumentException() {
+    // given
+    final Key key = accessKey;
+    final String token = "test";
+
+    // when
+    final Throwable throwable = catchThrowable(() -> jwtUtil.isTokenSignValid(key, token));
+
+    // then
+    assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+  }
+
+}
