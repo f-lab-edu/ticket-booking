f9b330f598ebf8f829aaecce03fc1513fa19fe52
hsw0905 <hsw0905@gmail.com>
2022-10-05 19:37:51 +0900

    refactor: varchar length 설정 추가

--- app/src/main/java/org/project/concert/domain/Concert.java
+++ app/src/main/java/org/project/concert/domain/Concert.java
@@ -1,6 +1,7 @@
 package org.project.concert.domain;
 
 import java.util.Objects;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,14 +14,18 @@ import org.project.common.domain.BaseTimeEntity;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "concerts")
-@Entity @Getter
+@Entity
+@Getter
 public class Concert extends BaseTimeEntity {
 
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(length = 80)
   private String title;
 
+  @Column(length = 120)
   private String description;
 
   public Concert(String title, String description) {
