58b8f9df54de5a552ecf1b3062fe858e2d923b99
tbonelee <chanho0325@gmail.com>
2022-10-12 04:41:19 +0900

    test: AuthToken 수정에 맞춰서 테스트 수정

--- app/src/test/java/org/project/domain/user/dto/OAuthLoginResponseTest.java
+++ app/src/test/java/org/project/domain/user/dto/OAuthLoginResponseTest.java
@@ -12,15 +12,15 @@ public class OAuthLoginResponseTest {
   @DisplayName("AuthTokens를 받으면 동일한 access, refresh를 가진 OAuthLoginResponse를 반환한다.")
   void testConstructor() {
     // given
-    Jwt accessToken = new Jwt(null, null, null);
-    Jwt refreshToken = new Jwt(null, null, null);
+    Jwt accessToken = new Jwt("access", null, null);
+    Jwt refreshToken = new Jwt("refresh", null, null);
     AuthTokens authTokens = new AuthTokens(accessToken, refreshToken);
 
     // when
     OAuthLoginResponse oAuthLoginResponse = new OAuthLoginResponse(authTokens);
 
     // then
-    assertThat(oAuthLoginResponse.getAccess()).isEqualTo("access");
-    assertThat(oAuthLoginResponse.getRefresh()).isEqualTo("refresh");
+    assertThat(oAuthLoginResponse.getAccess()).isEqualTo(accessToken.getToken());
+    assertThat(oAuthLoginResponse.getRefresh()).isEqualTo(refreshToken.getToken());
   }
 }
