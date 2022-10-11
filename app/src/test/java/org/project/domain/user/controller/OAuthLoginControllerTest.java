32989033b91f496e24107d58b44e2dbf53c94592
tbonelee <chanho0325@gmail.com>
2022-10-12 04:43:13 +0900

    test: Jpa 사용하지 않는 테스트 클래스에 Jpa context 모킹 주입

--- app/src/test/java/org/project/domain/user/controller/OAuthLoginControllerTest.java
+++ app/src/test/java/org/project/domain/user/controller/OAuthLoginControllerTest.java
@@ -17,10 +17,12 @@ import org.project.domain.user.service.OAuthLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest(controllers = OAuthLoginController.class)
+@MockBean(JpaMetamodelMappingContext.class)
 public class OAuthLoginControllerTest {
 
   @Autowired
