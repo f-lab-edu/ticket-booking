a6159048d6c00da560819db5a44cfc3a64ad043a
tbonelee <chanho0325@gmail.com>
2022-11-13 23:22:47 +0900

    fix: refres token repository (레디스 템플릿 사용)의 save() transactional하게 이루어지도록 수정
    - 리프레시 토큰 set과 expire 한 단위로 설정

--- app/src/main/java/org/project/configuration/RedisConfiguration.java
+++ app/src/main/java/org/project/configuration/RedisConfiguration.java
@@ -6,13 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableRedisRepositories
 public class RedisConfiguration {
 
   private final RedisProperties redisProperties;
@@ -25,6 +23,7 @@ public class RedisConfiguration {
   @Bean
   public RedisTemplate<String, Object> redisTemplate() {
     RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setEnableTransactionSupport(true);
     redisTemplate.setConnectionFactory(redisConnectionFactory());
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     redisTemplate.setValueSerializer(new StringRedisSerializer());
