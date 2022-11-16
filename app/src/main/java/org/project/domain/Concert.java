fe0a42aee5f8a736792219a48a4e51de6b243ecd
tbonelee <chanho0325@gmail.com>
2022-11-16 20:59:31 +0900

    feat: POST /api/v1/order/preoccupy api 추가

--- app/src/main/java/org/project/domain/Concert.java
+++ app/src/main/java/org/project/domain/Concert.java
@@ -1,7 +1,9 @@
 package org.project.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.project.exception.TicketNotFoundException;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "concert")
@@ -35,6 +38,17 @@ public class Concert extends BaseTimeEntity {
   public Concert(String title, String description) {
     this.title = title;
     this.description = description;
+  }
+
+  public List<Ticket> getAvailableTickets(Integer quantity) {
+    List<Ticket> ret = tickets.stream()
+        .filter(ticket -> ticket.getTicketOrder() == null)
+        .limit(quantity)
+        .collect(Collectors.toCollection(ArrayList::new));
+    if (ret.size() < quantity) {
+      throw new TicketNotFoundException(this);
+    }
+    return ret;
   }
 
   @Override
--- app/src/main/java/org/project/service/OrderService.java
+++ app/src/main/java/org/project/service/OrderService.java
@@ -1,24 +1,36 @@
 package org.project.service;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.apache.commons.lang3.NotImplementedException;
 import org.project.configuration.OrderProperties;
 import org.project.domain.Concert;
 import org.project.domain.Member;
+import org.project.domain.Ticket;
 import org.project.domain.TicketOrder;
 import org.project.dto.PreoccupyResult;
 import org.project.exception.ConcertNotFoundException;
+import org.project.exception.TicketNotFoundException;
 import org.project.repository.ConcertRepository;
+import org.project.repository.PreoccupyRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 
+  private final Clock clock;
   private final ConcertRepository concertRepository;
   private final OrderProperties orderProperties;
+  private final PreoccupyRepository preoccupyRepository;
 
-  public OrderService(ConcertRepository concertRepository, OrderProperties orderProperties) {
+  public OrderService(Clock clock, ConcertRepository concertRepository,
+      OrderProperties orderProperties,
+      PreoccupyRepository preoccupyRepository) {
+    this.clock = clock;
     this.concertRepository = concertRepository;
     this.orderProperties = orderProperties;
+    this.preoccupyRepository = preoccupyRepository;
   }
 
   public TicketOrder book(Long concertId, Member member) {
@@ -32,7 +44,21 @@ public class OrderService {
     Concert concert = concertRepository.findById(concertId)
         .orElseThrow(() -> new ConcertNotFoundException(concertId));
 
-    throw new NotImplementedException("Preoccupy not implemented");
+    // get a ticket
+    List<Ticket> availableTickets = concert.getAvailableTickets(1);
+
+    // find which is not preoccupied (처음 구현시 생각 못 한 부분)
+    Ticket ticket = availableTickets.stream()
+        .filter(t -> !preoccupyRepository.isPreoccupied(t.getId()))
+        .findFirst()
+        .orElseThrow(() -> new TicketNotFoundException(concert));
+
+    // preoccupy ticket
+    preoccupyRepository.save(ticket, member, orderProperties.getPreoccupyExpireTime());
+    LocalDateTime validUntil = LocalDateTime.now(clock)
+        .plusSeconds(orderProperties.getPreoccupyExpireTime());
+
+    return new PreoccupyResult(ticket.getId(), validUntil);
   }
 
 }
